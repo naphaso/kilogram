1826b8365bc7f1cd182200a59b5a5e5f2e3d9ccd
Igor Glotov <igor.n.glotov@gmail.com>
2013-12-05 23:43:31 +0700

    App bar in dialog/chat list

--- Telegram/UI/DialogList.xaml.cs
+++ Telegram/UI/DialogList.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 using Telegram.UI.Models;
 
 namespace Telegram.UI
@@ -26,6 +28,23 @@ namespace Telegram.UI
 
             items.Add(new DialogItem() { Avatar = "4", Preview = "Stay awhile and listen.", Timestamp = "1:21a", Title = "Decard Kain" });
             dialogList.ItemsSource = new ObservableCollection<DialogItem>(items.OrderBy(i => i.Timestamp));
+
+            BuildAppBar();
+        }
+
+        private void BuildAppBar() {
+            ApplicationBar = new ApplicationBar();
+            
+            var newButton = new ApplicationBarIconButton(new Uri("/Assets/UI/appbar.new.png", UriKind.Relative));
+            newButton.Text = "Create";
+
+            var searchButton = new ApplicationBarIconButton(new Uri("/Assets/UI/appbar.feature.search.png", UriKind.Relative));
+            searchButton.Text = "Search";
+
+            ApplicationBar.Buttons.Add(newButton);
+            ApplicationBar.Buttons.Add(searchButton);
+
+            ApplicationBar.MenuItems.Add(new ApplicationBarMenuItem("Settings"));
         }
     }
 }
