f4fb6dba985fba39ff599887285daae959eafb53
Igor Glotov <glotov@jelastic.com>
2013-12-08 18:22:39 +0700

    Contacts with sections

---
+++ Telegram/UI/Models/Users/UserItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.UI.Models.Users {
+    class UserItem {
+        public string Name { get; set; }
+        public string LastSeen { get; set; }
+
+        public bool Online { get; set; }
+
+        public bool AddressBookContact { get; set; }
+    }
+}
---
+++ Telegram/UI/StartPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Telegram.UI.Models;
+using Telegram.UI.Models.Users;
+
+namespace Telegram.UI
+{
+    public partial class StartPage : PhoneApplicationPage
+    {
+        private static DialogsModel _dialogs = null;
+
+        public static DialogsModel Dialogs {
+            get { return _dialogs ?? (_dialogs = new DialogsModel()); }
+        }
+
+        public StartPage()
+        {
+            InitializeComponent();
+            var items = new ObservableCollection<DialogItem> {
+                new DialogItem() {Avatar = "1", Preview = "Hello.", Timestamp = "11:21a", Title = "John Doe"},
+                new DialogItem() {Avatar = "2", Preview = "Hi there!", Timestamp = "9:56a", Title = "Jane Doe"},
+                new DialogItem() {Avatar = "3", Preview = "Stay awhile and listen.", Timestamp = "1:21a", Title = "Decard Kain"}
+            };
+
+            dialogList.ItemsSource = items;
+
+            items.Add(new DialogItem() { Avatar = "4", Preview = "Stay awhile and listen.", Timestamp = "1:21a", Title = "Decard Kain" });
+            dialogList.ItemsSource = new ObservableCollection<DialogItem>(items.OrderBy(i => i.Timestamp));
+
+            var users = new List<UserItem> {
+                new UserItem() {Name = "John Doe", Online = true},
+                new UserItem() {Name = "Jane Doe", Online = true},
+                new UserItem() {Name = "Decard Kain", Online = false, LastSeen = "19:33p"},
+                new UserItem() {Name = "Stanislav Ovsyannikov", AddressBookContact = true}
+            };
+
+            List<AlphaKeyGroup<UserItem>> userDataSource = AlphaKeyGroup<UserItem>.CreateGroups(users,
+                System.Threading.Thread.CurrentThread.CurrentUICulture,
+                (UserItem s) => { return s.Name; }, true);
+
+            var observableUsersSource = new ObservableCollection<AlphaKeyGroup<UserItem>>(userDataSource);
+
+            contactsList.ItemsSource = observableUsersSource;
+
+            BuildAppBar();
+        }
+
+        private void BuildAppBar() {
+            ApplicationBar = new ApplicationBar();
+            
+            var newButton = new ApplicationBarIconButton(new Uri("/Assets/UI/appbar.new.png", UriKind.Relative));
+            newButton.Text = "Create";
+
+            var searchButton = new ApplicationBarIconButton(new Uri("/Assets/UI/appbar.feature.search.png", UriKind.Relative));
+            searchButton.Text = "Search";
+
+            ApplicationBar.Buttons.Add(newButton);
+            ApplicationBar.Buttons.Add(searchButton);
+
+            ApplicationBar.MenuItems.Add(new ApplicationBarMenuItem("Settings"));
+        }
+    }
+}
